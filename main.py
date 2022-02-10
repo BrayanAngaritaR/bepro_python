503717f7ea16f6e0f6820638999486a16e9ace07
Brayan Angarita <brayanangarita11@gmail.com>
2022-02-10 09:45:21 -0500

    Requirements updated

--- main.py
+++ main.py
@@ -18,7 +18,14 @@ import time
 
 print('Abriendo sitio web')
 
-chrome_options=Options()
+#chrome_options=Options()
+#driver = webdriver.Chrome('chromedriver',options=chrome_options)
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--user-data-dir2=chrome-data")
-driver = webdriver.Chrome('chromedriver',options=chrome_options)
-driver.get('https://bepro.digital')
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+browser.get('https://bepro.digital')
